c0e35ed9557b694f2acce12b144dae516f54ab88
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-05-07 01:15:23 +0200

    Created basic app layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import React from 'react';
+
+import {
+  IonApp,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonSplitPane
+} from '@ionic/react';
+import Menu from './components/Menu';
+
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>trashfider app</h1>
-    </div>
+    <IonApp>
+      <IonSplitPane contentId="main">
+        <Menu />
+        <IonPage id="main">
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonMenuButton />
+              </IonButtons>
+              <IonTitle>Signup</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+        </IonPage>
+      </IonSplitPane>
+    </IonApp>
   );
 };
 
