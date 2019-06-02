11ff31776b752e377e82c341e56db72e7245813b
klapaucius4 <klapaucius.mk@gmail.com>
2019-06-03 00:50:40 +0200

    front-end mods v3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import React from 'react';
 
 import { IonApp } from '@ionic/react';
 
-import Start from './pages/Start';
-// import Menu from './components/Menu';
-
-import './App.css';
-
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
+import './App.css';
+
+import Start from './pages/Start';
+// import Menu from './components/Menu';
 
 const App = () => {
   return (
