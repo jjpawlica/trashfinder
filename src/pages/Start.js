2162d5d3965200f8082ebe61e56e9ad7f09b6e3d
klapaucius4 <klapaucius.mk@gmail.com>
2019-06-02 18:49:26 +0200

    #front-end of start page

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -6,20 +6,33 @@ import { IonContent, IonGrid, IonIcon, IonText, IonButton, IonRow, IonCol } from
 
 import './Start.css';
 
+import logo from '../images/logo.svg';
+import startImage from '../images/start-image.svg';
+
 const Start = () => {
   return (
     <IonContent fullscreen scroll-y="false">
       <IonGrid fixed className="content-wrapper">
         <IonRow align-items-stretch>
           <IonCol className="center-column">
-            <h1>Trashfinder</h1>
+            <img src={logo} className="App-logo" alt="Logo" />
           </IonCol>
         </IonRow>
-        <IonRow>
+        <IonRow align-items-stretch>
+          <IonCol className="center-column">
+            <h1>Zgłaszaj zanieczyszczone miejsca, sprzataj zgłoszone, oceniaj.</h1>
+          </IonCol>
+        </IonRow>
+        <IonRow align-items-stretch>
+          <IonCol className="center-column">
+            <img src={startImage} className="App-logo" alt="Start" />
+          </IonCol>
+        </IonRow>
+        {/* <IonRow>
           <IonCol className="center-column">
             <IonIcon name="leaf" className="logo" />
           </IonCol>
-        </IonRow>
+        </IonRow> */}
         <IonRow align-items-stretch>
           <IonCol className="center-column">
             <IonText className="center-text">
@@ -30,9 +43,9 @@ const Start = () => {
         </IonRow>
         <IonRow align-items-stretch>
           <IonCol>
-            <IonButton expand="block">Log In</IonButton>
-            <IonButton expand="block">Sign Up with Email</IonButton>
-            <IonButton expand="block">Sing up with Google</IonButton>
+            <IonButton expand="block">Logowanie</IonButton>
+            <IonButton expand="block">Rejestracja przez email</IonButton>
+            <IonButton expand="block">Rejestracja przez Google</IonButton>
           </IonCol>
         </IonRow>
       </IonGrid>
