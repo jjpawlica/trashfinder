ef848b235598694b9d0c24ed75d4654537f3f57e
klapaucius4 <klapaucius.mk@gmail.com>
2019-06-09 13:52:58 +0200

    weather v2

--- src/pages/MainPage/tabs/weather.js
+++ src/pages/MainPage/tabs/weather.js
@@ -1,19 +1,57 @@
 import React from 'react';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+// import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import { IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonList, IonItem, IonLabel, IonItemSliding, IonItemOption, IonItemOptions } from '@ionic/react';
+
 
 const WeatherTab = () => {
   return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Pogoda</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <h1>Weather</h1>
-      </IonContent>
-    </>
+  <>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Prognoza pogody</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    {/* <IonList>
+      <ion-list-header>
+        <ion-label>Items</ion-label>
+      </ion-list-header>
+      <IonItem> */}
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>Card Title</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+          </IonCardContent>
+        </IonCard>
+      {/* </IonItem>
+    </IonList> */}
+
+    <IonList>
+      <ion-list-header>
+        <ion-label>Prognoza dla największych miast w Polsce:</ion-label>
+      </ion-list-header>
+      <IonItem>
+        <IonLabel>Warszawa: </IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Kraków: </IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Gdańsk</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Poznań</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Wrocław</IonLabel>
+      </IonItem>
+    </IonList>
+  </>
   );
 };
 
