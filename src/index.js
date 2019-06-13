2240d96de0639c4a2a2d2d719e7ef81bfec230e2
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-06-13 22:25:35 +0200

    enabled ionic pwa-elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import App from './components/App';
 
 import Firebase from './components/Firebase/firebase';
@@ -14,4 +15,8 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
+
+// Call the element loader after the app has been rendered the first time
+defineCustomElements(window);
+
 serviceWorker.unregister();
